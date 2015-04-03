12076f437b1baf9b688cf327b049429b84f515fb
janocl <janocl84@gmail.com>
2015-04-03 16:59:14 -0300

    Agregado el ViewModel de Genero

---
+++ CodeFirstEF6/Models/GeneroViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstEF6.Models
+{
+    public class GeneroViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
