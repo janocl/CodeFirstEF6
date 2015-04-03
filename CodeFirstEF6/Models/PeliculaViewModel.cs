725cea4c614ccfa1a8dfe8e15649e058fe3b0a82
janocl <janocl84@gmail.com>
2015-04-03 16:58:36 -0300

    Agregado el ViewModel de Peliculas

---
+++ CodeFirstEF6/Models/PeliculaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstEF6.Models
+{
+    public class PeliculaViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Estreno { get; set; }
+        public string Duracion { get; set; }
+        public int Rating { get; set; }
+        public string Descripcion { get; set; }
+        public string Pais { get; set; }
+        public int IdGenero { get; set; }
+    }
+}
