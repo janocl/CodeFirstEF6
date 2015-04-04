53683245003ea2f77fd9f502a96cdbc4d683c27c
janocl <janocl84@gmail.com>
2015-04-04 01:07:52 -0300

    Agregado el DbContext

---
+++ CodeFirstEF6/DAL/CatalogoContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CodeFirstEF6.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace CodeFirstEF6.DAL
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext() : base("CatalogoContext")
+        {
+        }
+        
+        public DbSet<PeliculaViewModel> Peliculas { get; set; }
+        public DbSet<GeneroViewModel> Generos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
