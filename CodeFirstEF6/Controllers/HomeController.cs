37ff309fd63077277751c17ab75270cc0ec5af0a
janocl <janocl84@gmail.com>
2015-04-05 22:42:00 -0300

    Arreglada la vista del detalle y eliminacion de una propiedad

--- CodeFirstEF6/Controllers/HomeController.cs
+++ CodeFirstEF6/Controllers/HomeController.cs
@@ -8,10 +8,10 @@ namespace CodeFirstEF6.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
         public ActionResult About()
         {
